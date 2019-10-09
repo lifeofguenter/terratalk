33e3320dc86e8f785eba41f2c769032067002c6b
Günter Grodotzki <gunter@grodotzki.co.za>
2019-10-09 21:58:31 +0200

    make use of env vars (#2)
    * make use of env vars
    
    * add server

--- terratalk/__main__.py
+++ terratalk/__main__.py
@@ -1,6 +1,8 @@
-import click
-
+import os
+import re
 import subprocess
+
+import click
 
 from terratalk.bitbucket_server import BitbucketServer
 from terratalk.terraform import Terraform
@@ -20,6 +22,19 @@ def cli():
 @click.option('--repository-slug')
 @click.option('--pull-request-id')
 def comment(server, username, password, workspace, project_key, repository_slug, pull_request_id):
+
+    if server is None and project_key is None and repository_slug is None and pull_request_id is None and os.getenv('CHANGE_URL') is not None:
+        m = re.match(r'\Ahttps?://.*?)/projects/([^/]+)/repos/([^/]+)/pull-requests/(\d+)', os.getenv('CHANGE_URL'), re.IGNORECASE)
+        server = m.group(1)
+        project_key = m.group(2)
+        repository_slug = m.group(3)
+        pull_request_id = m.group(4)
+
+    if username is None and os.getenv('STASH_USER') is not None:
+        username = os.getenv('STASH_USER')
+
+    if password is None and os.getenv('STASH_PASS') is not None:
+        password = os.getenv('STASH_PASS')
 
     bs = BitbucketServer(base_url=server, username=username, password=password)
     bs.pr(project_key=project_key, repository_slug=repository_slug, pull_request_id=pull_request_id)
