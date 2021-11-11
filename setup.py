fd762a8221a53f62933e7c29470717fdd56d9545
lifeofguenter <gunter@grodotzki.com>
2021-11-11 19:43:11 +0100

    prepare 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='terratalk',
-    version='0.1.1',
+    version='0.2.0',
     author='Günter Grodotzki',
     author_email='gunter@grodotzki.com',
     description='A Terraform commentator.',
